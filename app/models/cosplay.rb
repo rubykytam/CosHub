2d5ce5aab68d3b7187e7e973992779b6ca51a10b
Nikita Nagras <snbnikita@gmail.com>
2024-02-19 16:44:34 +0900

    created cosplay and booking models

---
+++ app/models/cosplay.rb
@@ -0,0 +1,11 @@
+class Cosplay < ApplicationRecord
+  belongs_to :user
+  has_many :bookings
+
+  SIZE = ['XS', 'S', 'M', 'L', 'XL']
+  validates :size, presence: true
+  validates :size, inclusion: { in: SIZE }
+  validates :name, presence: true
+  validates :source_material, presence: true
+  validates :price, presence: true
+end
