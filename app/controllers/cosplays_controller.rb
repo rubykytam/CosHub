1028f47a0394cf4a0d05008912b1d2d9364de40f
justin etienne <juetienne7@gmail.com>
2024-02-20 17:13:52 +0900

    skip authentification for cosplays index and show

--- app/controllers/cosplays_controller.rb
+++ app/controllers/cosplays_controller.rb
@@ -1,5 +1,6 @@
 class CosplaysController < ApplicationController
   before_action :set_cosplay, only: [:show]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @cosplays = Cosplay.all
