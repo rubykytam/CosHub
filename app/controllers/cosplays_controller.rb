04c4f107ab1a1abbcdc33155bf1266a517dc59bf
justin etienne <juetienne7@gmail.com>
2024-02-20 14:06:40 +0900

    cosplay controller and show image

--- app/controllers/cosplays_controller.rb
+++ app/controllers/cosplays_controller.rb
@@ -9,9 +9,27 @@ class CosplaysController < ApplicationController
     @booking = Booking.new
   end
 
+  def new
+    @cosplay = Cosplay.new
+  end
+
+  def create
+    @cosplay = Cosplay.new(cosplay_params)
+    @cosplay.user = @user
+    if @cosplay.save
+        # redirect_to MY COSPLAYS ???
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_cosplay
     @cosplay = Cosplay.find(params[:id])
+  end
+
+  def cosplay_params
+    params.require(:cosplay).permit(:name, :size, :source_material, :price, :image)
   end
 end
