b5c9d49f163a3f3f01d908e295d5419173bc87f7
justin etienne <juetienne7@gmail.com>
2024-02-20 18:10:25 +0900

    changing seeds but not done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,23 +7,45 @@ User.destroy_all
 
 User.create(email: '123@gmail.com', password: '123123')
 
+COSPLAYS = [
+  {
+    name: "Dark Vador",
+    source_material: "Star Wars",
+    price: "2000",
+    size: "XS",
+    image_url: "https://newcossky.fr/cdn/shop/products/11006195-cosplaystyle2014.jpg?v=1684143610",
+    user: u
+  },
+  {
+
+  }
+]
+COSPLAYS.each do cosplay
+
 10.times do
   u = User.create!(
     email: Faker::Internet.email,
     password: '123123'
   )
   c = Cosplay.create!(
-    name: Faker::Artist.name,
-    source_material: Faker::Game.title,
-    size: Cosplay::SIZE.sample,
-    price: 30000,
-    user: u
+    name: cosplay[:name],
+    source_material: cosplay[:source_material],
+    size: cosplay[:size],
+    price: cosplay[:price],
+    user: User.all.sample
+
   )
   c.image.attach(
-    io:  URI.open('https://i.pinimg.com/564x/17/44/85/17448573378518e8d7dd86552639f923.jpg'),
+    io: URI.open(cosplay[:image_url]),
     filename: 'image.jpg',
-    content_type: "image/jpg"
+    content_type: 'image/jpg'
   )
+
+  # c.image.attach(
+  #   io:  URI.open('https://i.pinimg.com/564x/17/44/85/17448573378518e8d7dd86552639f923.jpg'),
+  #   filename: 'image.jpg',
+  #   content_type: "image/jpg"
+  # )
 end
 
 
